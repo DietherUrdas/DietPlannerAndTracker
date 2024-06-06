89472e2e61fa87a3c4183ead855914e8ff6a4d73
saromoAndreiLuis <165786140+saromoAndreiLuis@users.noreply.github.com>
2024-06-07 05:34:49 +0800

    5am commit

--- app/src/main/java/com/example/androidlabactivitysaromo/plan_dinner.java
+++ app/src/main/java/com/example/androidlabactivitysaromo/plan_dinner.java
@@ -1,7 +1,24 @@
 package com.example.androidlabactivitysaromo;
 
-public class plan_dinner {
-    public static void main(String[] args) {
-        System.out.println("breakfast");
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class plan_dinner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 }
